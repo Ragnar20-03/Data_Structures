f7959410f87f165d6edf06cc586fde989bae2af1
Ragnar20-03 <roshanpp20@gmail.com>
2024-04-20 01:42:05 +0530

    Identical Trees

---
+++ Tree/100_Same_Tree.cpp
@@ -0,0 +1,36 @@
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+ * };
+ */
+class Solution {
+public:
+    bool isSameTree(TreeNode* p, TreeNode* q) {
+        if ( p == nullptr || q == nullptr)
+        {
+            return p == q; // if p is numm & q not : return false AND Vice versa
+            /*
+            if (p == nullptr && q != nullptr) 
+            {
+                return false;
+            }
+            else if (p 1= nullptr && q == nullptr) 
+            {
+                return false;
+            }
+            else 
+            {
+                return true;
+            }
+            */
+        }
+
+        return (p -> val == q -> val) && isSameTree(p -> left , q->left) && isSameTree(p->right , q->right);
+    }
+};
