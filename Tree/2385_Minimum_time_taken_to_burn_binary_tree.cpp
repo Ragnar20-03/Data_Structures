b3c0c32eae76a3b1876a27de5a9e905ce5d4f3e7
Ragnar20-03 <roshanpp20@gmail.com>
2024-05-06 00:55:16 +0530

    Flag UNderstanding

--- Tree/2385_Minimum_time_taken_to_burn_binary_tree.cpp
+++ Tree/2385_Minimum_time_taken_to_burn_binary_tree.cpp
@@ -11,6 +11,7 @@
  */
 class Solution {
 public:
+// just write before doing it one function AND Practice of recurrsion
     TreeNode * findStart (TreeNode * root , int start)
     {
         if (root == nullptr)return nullptr;
@@ -27,14 +28,17 @@ public:
         }
         return nullptr ; 
     }
-    void MarkParent(TreeNode * root , unordered_map < TreeNode * ,TreeNode * > & parent_track)
+
+    // Mark parnt and also find the start Node 
+    TreeNode *  MarkParent(TreeNode * root , unordered_map < TreeNode * ,TreeNode * > & parent_track , int start)
     {
         queue < TreeNode *  > q ; 
         q.push(root );
-
+        TreeNode * startNode = nullptr;
         while (q.empty() == false )
         {
             TreeNode * node = q.front() ; 
+            if (node -> val == start) startNode = node ; 
             q.pop() ; 
             if (node -> left)
             {
@@ -47,12 +51,13 @@ public:
                 parent_track[node -> right] = node ; 
             }
         }
+        return startNode ; 
     }
     int amountOfTime(TreeNode* root, int start) {
         unordered_map < TreeNode* , TreeNode * > parent_track ; 
         unordered_map < TreeNode* , bool > visited ; 
-        MarkParent(root , parent_track);
-        TreeNode * startNode = findStart(root , start);
+
+        TreeNode * startNode = MarkParent(root , parent_track , start );
 
         queue < TreeNode * > q ; 
         q.push(startNode );
@@ -60,8 +65,12 @@ public:
         int ans = 0 ; 
         while (q.empty() == false )
         {
+            int flag = 0 ;  //  took it because , the last nodes will burn by their parent and our tree
+                            // burn completely succesfully but as they burn we pushesh them in queue and to avoid that extra iteration and make our ans count ++ again : 
+                            // to avoid it we will take flag  
+
             int size = q.size() ; 
-            ans ++ ; 
+            // ans ++ ;  //due to flag i had take it to down ( flag = if burn then increment ans)
 
             for (int i = 0 ; i < size ; i++ )
             {
@@ -70,22 +79,27 @@ public:
 
                 if (node -> left && visited[node -> left ] == false )
                 {
+                    flag =1 ; 
                     q.push(node -> left);
                     visited[node -> left] = true ; 
                 }
                 if (node -> right && visited[node -> right ] == false )
                 {
+                    flag = 1 ;
                     q.push(node -> right );
                     visited[node -> right ] = true ; 
                 }
                 if (parent_track[node ] && visited[parent_track[node]] == false )
                 {
+                    flag = 1 ; 
                     q.push(parent_track[node]);
                     visited[parent_track[node]]=true ; 
                 }
             }
+            if (flag) ans ++ ;  // in lastiteration it will not incerment so in final result we will not do (return ans -1 ; )
+
         }
-        return ans -1  ; 
+        return ans ; 
         
     }
 };
