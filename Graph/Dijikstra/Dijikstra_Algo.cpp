6055f1c5f02ff34a577ebf66992fc967ef9a7460
Ragnar20-03 <roshanpp20@gmail.com>
2024-05-27 17:51:40 +0530

    Dijikstra Algorithm

---
+++ Graph/Dijikstra/Dijikstra_Algo.cpp
@@ -0,0 +1,40 @@
+     #include<iostream>
+        using namespace  std ; 
+        
+     #include<vector>
+     #include<set>
+     vector <int> dijkstra(int V, vector<vector<int>> adj[], int S)
+    {
+        // Code here
+        set < pair <int , int > > st ; 
+        vector<int > distance ( V , 1e9);
+        distance[S] = 0 ; 
+        
+        st.insert({0 , S});
+        
+        while (st.empty() == false )
+        {
+            auto it = *(st.begin());
+            int node = it.second ; 
+            int dist = it.first ; 
+            st.erase(it);
+            
+            for (auto it : adj [ node])
+            {
+                int adjNode = it[0];
+                int weight = it[1];
+                
+                if (dist + weight < distance[adjNode])
+                {
+                    if (distance[adjNode] != 1e9)
+                    {
+                        st.erase({distance[adjNode] , adjNode});
+                    }
+                    
+                    distance[adjNode] = dist + weight ; 
+                    st.insert({distance[adjNode] , adjNode});
+                }
+            }
+        }
+        return distance ; 
+    }
