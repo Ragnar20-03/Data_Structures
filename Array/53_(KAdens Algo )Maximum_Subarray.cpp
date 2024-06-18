b6541396849eb66c1ccdb5db3ac919e74210737d
Ragnar20-03 <roshanpp20@gmail.com>
2024-06-18 14:48:39 +0530

    Kadens Algorithm for finding maximum sub array sum

--- Array/53_(KAdens Algo )Maximum_Subarray.cpp
+++ Array/53_(KAdens Algo )Maximum_Subarray.cpp
@@ -8,6 +8,26 @@
 #include<unordered_map>
 using namespace std;
 
+// OPTIMAL 
+//  KADENS ALGORITHM used for maximum sub array sum in linear time complexity o ( n ) and TC =  o (1 );
+
+class Solution {
+public:
+    int maxSubArray(vector<int>& nums) {
+        int ans = INT_MIN ; 
+        int sum = 0 ; 
+        for (int i = 0 ; i < nums.size() ; i++ )
+        {
+            sum = sum + nums[i];
+            ans = max ( ans , sum);
+            if (sum < 0 )
+            {
+                sum = 0 ; 
+            }
+        }
+        return ans ; 
+    }
+};
 
 // brute  Not Working 
 
