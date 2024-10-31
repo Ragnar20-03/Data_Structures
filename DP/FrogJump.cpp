e84a37a95b2c29ca78d977da36578098d3408bff
Ragnar20-03 <roshanpp20@gmail.com>
2024-10-31 16:36:12 +0530

    Dynamic Programming : Frog Jump : Tabulation

--- DP/FrogJump.cpp
+++ DP/FrogJump.cpp
@@ -63,3 +63,36 @@ int helper ( int i , vector<int > & height , vector<int > & dp )
     }
 
 // 3  Tabulation : convert it into the Tabulation .
+
+/*
+    Steps to convert from memoization to tabulation : 
+        1 ) see How many dp [] are used and initiallize them ; 
+        2 ) see the base case in memoization/rescursion . as it is bottom up approach 
+        3 ) see all the stuff perform on that index and do it ; 
+
+        according to 2 and 3rd contion design the for loop . 
+        ( ~ replace the functioncall with dp [ .... ]  for understanding .. now )
+
+
+*/
+ int minimumEnergy(vector<int>& height, int n) {
+        // Code here
+        vector<int> dp ( n , -1 );
+        dp [ 0 ] = 0 ; 
+        
+        for (int i= 1 ; i < n ; i++ )
+        {
+            //fs = first step ; 
+            int fs = dp[i-1] + abs ( height [ i ] -  height[i-1]) ; 
+            if ( i > 1 )
+            {
+                //ss = second step ; 
+                int ss  = dp[i-2] + abs ( height[i]-height[i-2]);
+                dp[i] = min ( fs , ss);
+            }
+            else {
+                dp [ i ] = fs;
+            }
+        }
+            return dp [ n - 1  ];
+    }
